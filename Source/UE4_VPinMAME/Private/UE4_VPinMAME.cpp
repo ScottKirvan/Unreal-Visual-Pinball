df36f692114b61b95a0ce5c8e2e8a33ec20e052d
scottkirvan <scottkirvan@gmail.com>
2021-03-06 18:46:57 -0800

    WIP -- safety checkin - added a menu to the editor - WAT?!

--- Source/UE4_VPinMAME/Private/UE4_VPinMAME.cpp
+++ Source/UE4_VPinMAME/Private/UE4_VPinMAME.cpp
@@ -4,9 +4,28 @@
 
 #define LOCTEXT_NAMESPACE "FUE4_VPinMAMEModule"
 
+TSharedRef<FWorkspaceItem> FUE4_VPinMAMEModule::MenuRoot = FWorkspaceItem::NewGroup(FText::FromString("Menu Root"));
+
 void FUE4_VPinMAMEModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	if (!IsRunningCommandlet())
+	{
+		FLevelEditorModule& LevelEditorModule = FModuleManager::LoadModuleChecked<FLevelEditorModule>("LevelEditor");
+		LevelEditorMenuExtensibilityManager = LevelEditorModule.GetMenuExtensibilityManager();
+		MenuExtender = MakeShareable(new FExtender);
+		MenuExtender->AddMenuBarExtension("Window", EExtensionHook::After, NULL, FMenuBarExtensionDelegate::CreateRaw(this, &FUE4_VPinMAMEModule::MakePulldownMenu));
+		LevelEditorMenuExtensibilityManager->AddExtender(MenuExtender);
+	}
+
+		if (!IsRunningCommandlet())
+		{
+			AddModuleListeners();
+			for (int32 i = 0; i < ModuleListeners.Num(); ++i)
+			{
+				ModuleListeners[i]->OnStartupModule();
+			}
+		}
 	
 }
 
@@ -14,9 +33,44 @@ void FUE4_VPinMAMEModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
+		for (int32 i = 0; i < ModuleListeners.Num(); ++i)
+		{
+			ModuleListeners[i]->OnShutdownModule();
+		}
 	
 }
+
+void FUE4_VPinMAMEModule::AddMenuExtension(const FMenuExtensionDelegate &extensionDelegate, FName extensionHook, const TSharedPtr<FUICommandList> &CommandList, EExtensionHook::Position position)
+{
+	MenuExtender->AddMenuExtension(extensionHook, position, CommandList, extensionDelegate);
+}
+
+void FUE4_VPinMAMEModule::MakePulldownMenu(FMenuBarBuilder &menuBuilder)
+{
+	menuBuilder.AddPullDownMenu(
+		FText::FromString("VPinMAME"),
+		FText::FromString("Open the VPinMAME options menu"),
+		FNewMenuDelegate::CreateRaw(this, &FUE4_VPinMAMEModule::FillPulldownMenu),
+		"Example",
+		FName(TEXT("ExampleMenu"))
+	);
+}
+
+void FUE4_VPinMAMEModule::FillPulldownMenu(FMenuBuilder &menuBuilder)
+{
+	menuBuilder.BeginSection("ExampleSection", FText::FromString("Section 1"));
+	menuBuilder.AddMenuSeparator(FName("Section_1"));
+	menuBuilder.EndSection();
+
+	menuBuilder.BeginSection("ExampleSection", FText::FromString("Section 2"));
+	menuBuilder.AddMenuSeparator(FName("Section_2"));
+	menuBuilder.EndSection();
+}
+
+
 
 #undef LOCTEXT_NAMESPACE
 	
 IMPLEMENT_MODULE(FUE4_VPinMAMEModule, UE4_VPinMAME)
+
+
--- Source/UE4_VPinMAME/Public/UE4_VPinMAME.h
+++ Source/UE4_VPinMAME/Public/UE4_VPinMAME.h
@@ -3,12 +3,41 @@
 #pragma once
 
 #include "Modules/ModuleManager.h"
+#include "UnrealEd.h"
+#include "SlateBasics.h"
+#include "SlateExtras.h"
+#include "Editor/LevelEditor/Public/LevelEditor.h"
+#include "Editor/PropertyEditor/Public/PropertyEditing.h"
+#include "IAssetTypeActions.h"
+
+class IVPinMAMEModuleListenerInterface
+{
+public:
+	virtual void OnStartupModule() {};
+	virtual void OnShutdownModule() {};
+};
 
 class FUE4_VPinMAMEModule : public IModuleInterface
 {
 public:
 
-	/** IModuleInterface implementation */
-	virtual void StartupModule() override;
-	virtual void ShutdownModule() override;
-};
+	virtual void StartupModule();
+	virtual void ShutdownModule();
+
+	virtual void AddModuleListeners() {};
+
+
+
+	void AddMenuExtension(const FMenuExtensionDelegate &extensionDelegate, FName extensionHook, const TSharedPtr<FUICommandList> &CommandList = NULL, EExtensionHook::Position position = EExtensionHook::Before);
+
+protected:
+	TArray<TSharedRef<IVPinMAMEModuleListenerInterface>> ModuleListeners;
+	TSharedPtr<FExtender> MenuExtender;
+
+	TSharedPtr<FExtensibilityManager> LevelEditorMenuExtensibilityManager;
+
+	static TSharedRef<FWorkspaceItem> MenuRoot;
+
+	void MakePulldownMenu(FMenuBarBuilder &menuBuilder);
+	void FillPulldownMenu(FMenuBuilder &menuBuilder);
+ };
